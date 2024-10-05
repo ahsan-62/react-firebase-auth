05640c43d90d7d3478e19a2b037e31279a4703eb
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-10-05 23:22:56 +0600

    Login with Google and Github (Firebase)

--- src/__steps.js
+++ src/__steps.js
@@ -5,6 +5,20 @@
 4.install firebase: npm install firebase
 5.create firebase.init.js file (see firebase.init.js)
 6.DANGER: Do not share or Commit your config file.  firebase.init.js file.
+7.Visit:Go to Documentation>Build>Authentication>web>Get started.
+8.Export app from firebase.init.js file: export default app;
+9.Login JSX: import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+10.Create const auth = getAuth(app);
+11.Create const provider = new GoogleAuthProvider();
+12.use signInWithPopup(auth, provider);
+13.Active sign in method(google,facebook,github,etc.)
+=> Build>Authentication>web>Sign in methods
+=>Build>Authentication>setting>Authorized Domain
 
+                ..........................
+                    For Github Login
+                ..........................
+
+14.
 
 **/
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,31 +1,75 @@
 
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
-
+    const [user, setUser] =useState(null);
     const auth=getAuth(app); 
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider;
+
 
     const loginWithGoogle=()=>{
         // console.log("login with google")
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
         .then(result => {
 
-            const user = result.user;
+            const loggedInUser = result.user;
             console.log(user);
+            setUser(loggedInUser);
             
         })
         .catch(error => {
-            console.log("error", error.message);
-            
+            console.log("error", error.message);  
         })
     }
+
+
+    const logOutGoogle=()=>{
+        signOut(auth)
+        .then((result) => {
+            // Sign-out successful.
+            console.log(result);
+            setUser(null);
+        })
+        .catch(error => {
+            console.log("error", error.message);  
+        })  
+    }
+
+
+    const loginWithGithub=()=>{
+        signInWithPopup(auth, githubProvider)
+        .then(result => {
+            const loggedInUser = result.user;
+            console.log(user);
+            setUser(loggedInUser);
+            
+        })
+        .catch(error => {
+            console.log("error", error.message);  
+        })
+    }
+
+    
     return (
         <div>
-            <button onClick={loginWithGoogle} type="button">Sign in With Login</button>
+            <h3>
+            {user ? <button onClick={logOutGoogle}>Log Out</button> : <>
+                <button onClick={loginWithGoogle}>Google Login</button> <button onClick={loginWithGithub}>Github Login</button></>}
+            </h3>
+
+            <h3>
+                {
+                    user&&`Welcome ${user.displayName}`
+                }
+            </h3>
+
+
+            
         </div>
     );
-};
+};;
 
 export default Login;
